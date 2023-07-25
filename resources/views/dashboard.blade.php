c8ef1858a255abc88f612950ee15fe7600b60b95
galdinoM <moises_galdino@hotmail.com>
2023-07-25 14:50:54 -0300

    solução de get

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,36 +17,6 @@
                                     <x-user-card :user="$user" />
                                 @endforeach
                             </div>
-
-                            <div x-data="{ isOpen: false, user: {} }">
-                                <div class="border border-gray-300 p-4 mb-4 rounded-lg">
-                                    <p class="text-lg font-semibold">{{ $user->name }}</p>
-                                    <p class="text-gray-600">{{ $user->email }}</p>
-                                    <button
-                                        @click="isOpen = true; user = {{ json_encode($user) }}"
-                                        class="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded focus:outline-none"
-                                    >
-                                        Ver detalhes
-                                    </button>
-                                </div>
-
-                                <!-- Modal -->
-                                <div x-show="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                                    <div @click.away="isOpen = false" class="bg-white p-4 rounded-lg shadow-md">
-                                        <p class="text-lg font-semibold" x-text="user.name"></p>
-                                        <p class="text-gray-600" x-text="user.email"></p>
-                                        <p class="text-gray-600" x-text="user.telefone ? 'Telefone: ' + user.telefone : ''"></p>
-                                        <p class="text-gray-600" x-text="user.cep ? 'CEP: ' + user.cep : ''"></p>
-                                        <p class="text-gray-600" x-text="user.logradouro ? 'Logradouro: ' + user.logradouro : ''"></p>
-                                        <p class="text-gray-600" x-text="user.complemento ? 'Complemento: ' + user.complemento : ''"></p>
-                                        <p class="text-gray-600" x-text="user.bairro ? 'Bairro: ' + user.bairro : ''"></p>
-                                        <p class="text-gray-600" x-text="user.uf ? 'UF: ' + user.uf : ''"></p>
-                                        <p class="text-gray-600" x-text="user.localidade ? 'Localidade: ' + user.localidade : ''"></p>
-                                        <p class="text-gray-600" x-text="user.updated_at ? 'Data/Hora: ' + formatDate(user.updated_at) : ''"></p>
-                                        <button @click="isOpen = false" class="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded focus:outline-none">Fechar</button>
-                                    </div>
-                                </div>
-                            </div>
                             <script>
                                 function formatDate(dateTimeString) {
                                     if (!dateTimeString) return '';
--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -10,7 +10,7 @@
 
 <body class="bg-gray-900 flex items-center justify-center min-h-screen">
     <div class="text-center p-6 rounded-lg bg-white shadow-md max-w-md animate-fade-in">
-        <h1 class="text-3xl font-semibold mb-4">Bem-vindo ao Meu Site</h1>
+        <h1 class="text-3xl font-semibold mb-4">Bem-vindo!!</h1>
         <p class="text-lg mb-4">Para acessar, clique no botão abaixo para fazer o login:</p>
         <a href="{{ route('login') }}" class="block">
             <button class="px-4 py-2 rounded-md bg-green-600 text-white">Fazer Login</button>
@@ -21,12 +21,10 @@
             0% {
                 opacity: 0;
             }
-
             100% {
                 opacity: 1;
             }
         }
-
         .animate-fade-in {
             animation: fade-in 1s ease-in-out;
         }
